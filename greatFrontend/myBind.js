841ae05dbaf541b8e8cccb908cd6eb44ed6ec08a
kieter <balisnom@ualberta.ca>
2024-09-25 14:26:21 -0700

    feat: myBind

---
+++ greatFrontend/myBind.js
@@ -0,0 +1,11 @@
+/**
+ * @param {any} thisArg
+ * @param {...*} argArray
+ * @return {Function}
+ */
+Function.prototype.myBind = function (thisArg, ...argArray) {
+    const originalMethod = this;
+    return function (...args) {
+        return originalMethod.apply(thisArg, [...argArray, ...args])
+    }
+};
