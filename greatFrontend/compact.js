0afb95bd2b38fa64746b778df9c1320bd70191cb
kieter <balisnom@ualberta.ca>
2024-10-15 16:44:19 -0700

    feat: compact

---
+++ greatFrontend/compact.js
@@ -0,0 +1,11 @@
+/**
+ * @param {Array} array: The array to compact.
+ * @return {Array} Returns the new array of filtered values.
+ */
+export default function compact(array) {
+    return array.filter((element, i) => {
+        if (!!element) {
+            return element
+        }
+    })
+}
