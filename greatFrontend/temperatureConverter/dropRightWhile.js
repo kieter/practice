143c730edffca8810b15b8ce05d54c9c5605af57
kieter <balisnom@ualberta.ca>
2024-10-16 16:10:42 -0700

    feat: dropRightWhile

---
+++ greatFrontend/temperatureConverter/dropRightWhile.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Array} array - The array to iterate over.
+ * @param {Function} predicate - The function invoked per iteration.
+ * @return {Array} Returns the slice of `array`.
+ */
+export default function dropRightWhile(array, predicate) {
+    let index = array.length - 1;
+
+    while (index >= 0 && predicate(array[index], index, array)) {
+        index--;
+    }
+
+    return array.slice(0, index + 1);
+}
